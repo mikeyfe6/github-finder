eeda9b862c9e51e286cc814f351072d89ff2012b
Mikey <mhmfransman@gmail.com>
2019-06-04 20:10:45 +0200

    Meanwhile..

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Hello van Mikey man..</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar />
+        <UserItem />
+      </div>
+    );
+  }
 }
 
 export default App;
